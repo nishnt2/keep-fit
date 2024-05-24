296cc0ea55bc6378650abe82072c3f10a65ec65c
nishnt2 <patilnishant222@gmail.com>
2024-05-24 15:30:45 +0530

    project base setup

---
+++ src/app/registration/page.js
@@ -0,0 +1,11 @@
+'use client';
+
+import Form from '../components/Form/Form';
+import styles from './page.module.css';
+export default function Registration() {
+  return (
+    <div className={styles.main}>
+      <Form />
+    </div>
+  );
+}
